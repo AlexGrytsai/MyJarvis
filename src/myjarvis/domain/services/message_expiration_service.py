3788920d0e8732a08ed16d542f36bce98091125e
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-26 11:30:09 +0300

    feat: Implement MessageExpirationService
    This commit introduces the `MessageExpirationService` responsible for removing expired messages from a `MessageCollection`.
    
    The service iterates through the messages, checks each message's expiration status based on its timestamp and a provided timeout, and removes expired messages accordingly. The implementation includes the `remove_expired_messages` method and a helper method `_is_message_expired` for checking individual message expiration.

--- src/myjarvis/domain/services/message_expiration_service.py
+++ src/myjarvis/domain/services/message_expiration_service.py
@@ -1,2 +1,39 @@
+from datetime import datetime
+from typing import Tuple, Optional, List, TypeAlias
+
+from src.myjarvis.domain.value_objects import MessageCollection, Message
+
+Errors: TypeAlias = Optional[List[str]]
+
+
 class MessageExpirationService:
-    pass
+    """
+    This service is responsible for removing expired messages from
+    a collection.
+
+    It defines a domain logic for checking if a message is expired based
+    on its timestamp and a given timeout value. The expiration check is done
+    by comparing the difference between the current time and the message's
+    timestamp with the given timeout value.
+    """
+
+    def remove_expired_messages(
+        self, messages_collection: MessageCollection, timeout: int
+    ) -> Tuple[MessageCollection, Errors]:
+        """
+        Removes expired messages from the collection and returns a new
+        """
+        if not timeout:
+            return messages_collection, None
+
+        ids_to_remove = [
+            message_id
+            for message_id, message in messages_collection.messages.items()
+            if self._is_message_expired(message, timeout)
+        ]
+
+        return messages_collection.partial_remove(ids_to_remove)
+
+    @staticmethod
+    def _is_message_expired(message: Message, timeout: int) -> bool:
+        return (datetime.now() - message.timestamp).total_seconds() > timeout
